d88e52201e96299262e70c62bc9bdf12ab95ae2c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-07-24 09:24:10 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,108 @@
+#    Copyright (C) 2021 - Infinity Bots
+#    This programme is a part of Infinity Bots
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import logging
+import requests
+import aiohttp
+import youtube_dl
+from pyrogram import filters, Client, idle
+from youtube_search import YoutubeSearch
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from config import Config
+
+# logging
+bot = Client(
+   "Song Downloader",
+   api_id=Config.APP_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.TG_BOT_TOKEN,
+)
+
+
+@bot.on_message(filters.command("start") & ~filters.edited)
+async def start(_, message):
+   if message.chat.type == 'private':
+       await message.reply("**Hey There, I'm a song downloader bot. A bot by @Infinity_Bots.\nUsage:** `/song [query]`",   
+                            reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Dev", url="https://t.me/Infinity_Bots"),
+                                        InlineKeyboardButton(
+                                            "Source", url="https://github.com/imjanindu/jesongbot")
+                                    ]]
+                            ))
+   else:
+      await message.reply("**Song downloader is online ✨**")
+
+
+@bot.on_message(filters.command("song") & ~filters.edited)
+async def song(_, message):
+    if len(message.command) < 2:
+       return await message.reply("**Usage:**\n - `/song [query]`")
+    query = message.text.split(None, 1)[1]
+    shed = message.reply("🔎 Finding the song...")
+    ydl_opts = {
+       "format": "bestaudio[ext=m4a]",
+       "geo-bypass": True,
+       "nocheckcertificate": True,
+       "outtmpl": "downloads/%(id)s.%(ext)s",
+       }
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+        channel = results[0]["channel"]
+    except Exception as e:
+        shed.edit(
+            "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
+        )
+        print(str(e))
+        return
+    shed.edit("📥 Downloading...")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = '@JEBotZ'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        shed.edit("📤 Uploading...")
+        s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur, performer=channel)
+        shed.delete()
+    except Exception as e:
+        shed.edit("❌ Error")
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
+
+bot.start()
+idle()
