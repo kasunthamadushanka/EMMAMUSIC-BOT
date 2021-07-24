b84264078d093bc2ab28872a6b2dd6d23ac6a2ec
KASU BRO <79846047+kasunthamadushanka@users.noreply.github.com>
2021-07-24 14:15:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ bot = Client(
 @bot.on_message(filters.command("start") & ~filters.edited)
 async def start(_, message):
    if message.chat.type == 'private':
-       await message.reply("**Hey There, I'm a ✨ 𝐄𝐌𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓 ✨(https://telegra.ph/file/e561880f97698b813a688.jpg).\n A bot by @epusthakalaya_Bots.\nUsage:** `Eg:- /song Bad Habits`",   
+       await message.reply("**Hey There, I'm a ✨ 𝐄𝐌𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓 ✨ https://telegra.ph/file/e561880f97698b813a688.jpg.\n A bot by @epusthakalaya_Bots.\nUsage:** `Eg:- /song Bad Habits`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
