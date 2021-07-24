ef89a6dd8e77dbdcd1c1a7156479f049e54c48cb
KASU BRO <79846047+kasunthamadushanka@users.noreply.github.com>
2021-07-24 13:28:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,7 +86,7 @@ async def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @epusthakalaua_bots**"
+        rep = "🎵 Uploaded by @epusthakalaua_bots"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
