209d361c80fbf8180f4462111dd025ab77273fc4
KASU BRO <79846047+kasunthamadushanka@users.noreply.github.com>
2021-07-24 13:25:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ bot = Client(
 @bot.on_message(filters.command("start") & ~filters.edited)
 async def start(_, message):
    if message.chat.type == 'private':
-       await message.reply("**Hey There, I'm a ✨ 𝐄𝐌𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓 ✨. A bot by @epusthakalaya_Bots.\nUsage:** `/song [query]`",   
+       await message.reply("**Hey There, I'm a ✨ 𝐄𝐌𝐌𝐀 𝐌𝐔𝐒𝐈𝐂 𝐁𝐎𝐓 ✨.\n A bot by @epusthakalaya_Bots.\nUsage:** `/song [query]`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                         InlineKeyboardButton(
@@ -86,7 +86,7 @@ async def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "@epusthakalaua_bots"
+        rep = "**🎵 Uploaded by @epusthakalaua_bots**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
